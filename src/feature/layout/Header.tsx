c932cb7e250625388440a775b448ffaa9e8ce21b
SomaTakata <somatakata@gmail.com>
2024-09-22 19:25:11 +0900

    Header responsive対応

--- src/feature/layout/Header.tsx
+++ src/feature/layout/Header.tsx
@@ -5,13 +5,18 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 const Header = () => {
   return (
     <header className="py-5 px-10 flex items-center justify-between border-b-[3px] border-black">
-      <div className="flex gap-6 items-center">
+      <div className="flex gap-6 items-center justify-center lg:justify-normal w-full">
+        {/* 名前は常に表示 */}
         <h1 className="font-roboto font-black text-[26px]">SOMA TAKATA</h1>
-        <span className="font-roboto font-semibold text-[11px] w-72 flex flex-wrap">
+        
+        {/* この説明部分は、モバイルやタブレットでは非表示 */}
+        <span className="font-roboto font-semibold text-[11px] w-72 lg:flex flex-wrap hidden">
           This is the portfolio site of Soma Takata, a Japanese man Software Engineer based in Saitama. I like Next.js.
         </span>
       </div>
-      <div className="flex gap-12">
+      
+      {/* この部分もモバイルやタブレットでは非表示 */}
+      <div className="lg:flex gap-12 hidden ">
         <nav className="flex gap-8 font-bold items-center">
           <div className="font-roboto">WORK</div>
           <div className="font-roboto">PROFILE</div>
