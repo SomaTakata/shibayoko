142c29e0164af579dedbc88caf0d37a5ba132c9d
SomaTakata <somatakata@gmail.com>
2024-09-23 15:13:57 +0900

    Header　レスポンシブ対応

--- src/feature/layout/Header.tsx
+++ src/feature/layout/Header.tsx
@@ -4,25 +4,25 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Header = () => {
   return (
-    <header className="py-5 px-10 flex items-center justify-between border-b-[3px] border-black">
-      <div className="flex gap-6 items-center justify-center lg:justify-normal w-full">
+    <header className="flex items-center justify-between border-b-[3px] border-black px-10 py-5">
+      <div className="flex w-full items-center justify-center gap-6 lg:justify-normal">
         {/* 名前は常に表示 */}
-        <h1 className="font-roboto font-black text-3xl">SOMA TAKATA</h1>
+        <h1 className="font-roboto text-3xl font-black">SOMA TAKATA</h1>
         
         {/* この説明部分は、モバイルやタブレットでは非表示 */}
-        <span className="font-roboto font-semibold text-[11px] w-72 lg:flex flex-wrap hidden">
+        <span className="hidden w-72 flex-wrap font-roboto text-[11px] font-semibold lg:flex">
           This is the portfolio site of Soma Takata, a Japanese man Software Engineer based in Saitama. I like Next.js.
         </span>
       </div>
       
       {/* この部分もモバイルやタブレットでは非表示 */}
-      <div className="lg:flex gap-12 hidden ">
-        <nav className="flex gap-8 font-bold items-center">
+      <div className="hidden gap-12 lg:flex ">
+        <nav className="flex items-center gap-8 font-bold">
           <div className="font-roboto">WORK</div>
           <div className="font-roboto">PROFILE</div>
           <div className="font-roboto">NEWS</div>
         </nav>
-        <div className="flex gap-4 items-center">
+        <div className="flex items-center gap-4">
           <Link href="/ja">
             <GitHubIcon />
           </Link>
