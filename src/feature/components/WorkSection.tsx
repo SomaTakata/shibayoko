8dc547b89a77bf32b3e8168258d5b26e99b1cd4f
SomaTakata <somatakata@gmail.com>
2024-09-23 15:13:43 +0900

    WorkSectionの追加

---
+++ src/feature/components/WorkSection.tsx
@@ -0,0 +1,34 @@
+import ViewAll from "./ViewAll";
+import WorkCard from "./WorkCard";
+
+const workItems = [
+  { title: 'フロントエンド', year: '2023', projectName: 'NginLink' },
+  { title: 'フロントエンド', year: '2023', projectName: 'NginLink' },
+  { title: 'フロントエンド', year: '2023', projectName: 'NginLink' },
+  { title: 'フロントエンド', year: '2023', projectName: 'NginLink' },
+  { title: 'フロントエンド', year: '2023', projectName: 'NginLink' },
+  { title: 'フロントエンド', year: '2023', projectName: 'NginLink' },
+];
+
+const WorkSection = () => (
+  <section className="flex flex-col gap-8 border-t-[3px] border-black px-5 py-8 sm:gap-12 sm:px-10 sm:py-14 md:px-14 md:py-32 lg:gap-16">
+    <div className="flex flex-col gap-6 sm:gap-8 lg:gap-10">
+      <h2 className="font-roboto text-5xl font-black tracking-widest text-white text-stroke">WORK</h2>
+
+      {/* WorkCardを動的に描画 */}
+      <div className="grid w-full grid-cols-2 gap-8 lg:grid-cols-3 xl:grid-cols-4">
+        {workItems.map((work, index) => (
+          <WorkCard
+            key={index}
+            title={work.title}
+            year={work.year}
+            projectName={work.projectName}
+          />
+        ))}
+      </div>
+      </div>
+      <ViewAll />
+  </section>
+);
+
+export default WorkSection;
