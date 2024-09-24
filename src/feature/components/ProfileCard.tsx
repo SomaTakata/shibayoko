74367bdbffd53c78a230f12933df0f7c69ec1c30
SomaTakata <somatakata@gmail.com>
2024-09-24 10:01:55 +0900

    Profile Sctionコンポーネントの実装

--- src/feature/components/ProfileCard.tsx
+++ src/feature/components/ProfileCard.tsx
@@ -1,28 +1,27 @@
 import Image from 'next/image';
-import EastIcon from '@mui/icons-material/East';
 import ViewAll from './ViewAll'; // ViewAllコンポーネントをインポート
 
 const ProfileCard = () => {
   return (
-    <div className="border-2 border-black rounded-xl p-10 max-w-sm mx-auto bg-white">
+    <div className="mx-auto max-w-sm rounded-xl border-2 border-black bg-white p-10">
       {/* イラストの部分 */}
-      <div className="flex justify-center mb-4">
-        <div className="relative w-48 h-48 rounded-full overflow-hidden border-2 border-black">
+      <div className="mb-4 flex justify-center">
+        <div className="relative size-48 overflow-hidden rounded-full border-2 border-black">
           <Image
             src="/avatar.png" 
             alt="Profile Image"
-            layout="fill"
-            objectFit="cover"
+            fill
+            className='object-contain'
           />
         </div>
       </div>
 
       {/* プロフィール情報 */}
       <div className="text-center">
-        <h2 className="text-4xl font-roboto font-bold text-white text-stroke-1 tracking-widest mt-3">PROFILE</h2>
-        <p className="text-sm font-roboto font-bold text-black mt-3">Web Engineer</p>
-        <h1 className="text-3xl font-roboto font-extrabold mt-1">SOMA TAKATA</h1>
-        <p className="text-sm text-start text-black font-roboto font-semibold mt-3">
+        <h2 className="mt-3 font-roboto text-4xl font-bold tracking-widest text-white text-stroke-1">PROFILE</h2>
+        <p className="mt-3 font-roboto text-sm font-bold text-black">Web Engineer</p>
+        <h1 className="mt-1 font-roboto text-3xl font-extrabold">SOMA TAKATA</h1>
+        <p className="mt-3 text-start font-roboto text-sm font-semibold text-black">
           HELLO WORLD, This is the portfolio site of Soma Takata, a Japanese man web engineer based in Tokyo.
           I am a student of SIT. 
         </p>
---
+++ src/feature/components/ProfileSection.tsx
@@ -0,0 +1,13 @@
+import ProfileCard from "./ProfileCard";
+
+const ProfileSection = () => {
+  return (
+    <section>
+    <div className="-z-10 flex h-[750px] items-center justify-center bg-gray-100">
+    <ProfileCard />
+    </div>
+  </section>
+  );
+};
+
+export default ProfileSection;
