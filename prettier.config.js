50a6599954c2cd91bc29860dd73efad1d8ad61b7
SomaTakata <somatakata@gmail.com>
2024-09-24 18:05:22 +0900

    formatterの修正

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: true,
+  arrowParens: "always",
+  trailingComma: "all",
+};
